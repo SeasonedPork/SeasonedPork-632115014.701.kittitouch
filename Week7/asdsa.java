b43cef83e9f8d38b9c189a0a11b58d5de2644ad1
Poiller <yanawarut_kanwana@elearning.cmu.ac.th>
2021-01-13 22:37:07 +0700

    commit

---
+++ Week7/asdsa.java
@@ -0,0 +1,11 @@
+package Week7;
+
+public class asdsa {
+    public static void main(String[]args)
+    {
+        System.out.println("404 file not found!!!");
+    }
+    //due to i have my code in the main CMU pc so yes pofessor i have nothing to send you 
+    //but if you wanty me to send it please reply in this comment
+    
+}
